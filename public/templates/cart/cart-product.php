d505d267e311e34a3206dc6f422677c4ef8622b5
Cheryl Yu <cyu@digitalriver.com>
2019-09-09 17:00:57 +0800

    Fix permalink issue for general products

--- public/templates/cart/cart-product.php
+++ public/templates/cart/cart-product.php
@@ -20,6 +20,7 @@ $product_sku = $line_item['product']['id'];
 $sale_price = $line_item['pricing']['formattedSalePriceWithQuantity'];
 $price = $line_item['pricing']['formattedListPriceWithQuantity'];
 $product_qty = $line_item['quantity'];
+$is_variation = isset( $line_item['product']['parentProduct'] );
 
 
 ?>
@@ -32,7 +33,7 @@ $product_qty = $line_item['quantity'];
 
         <div class="dr-product__info">
 
-            <a class="product-name" href="<?php echo get_permalink( drgc_get_product_by_sku( $product_sku, true ) ); ?>" >
+            <a class="product-name" href="<?php echo get_permalink( drgc_get_product_by_gcid( $product_sku, $is_variation ) ); ?>" >
                 <?php echo $product_name; ?>
             </a>
             <div class="product-sku">
